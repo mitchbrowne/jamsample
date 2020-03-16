74c79660dbf34ffc71c9f5e6ff1ab9b0c1719b7d
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-03-16 13:30:32 +1100

    Added functionality to new sample page

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,21 +1,32 @@
 class SamplesController < ApplicationController
   before_action :check_for_login
 
+  def index
+    @samples = @current_user.samples
+  end
+
   def new
     @sample = Sample.new
+    @collections = @current_user.collections
   end
 
   def create
-    
+    sample = Sample.create sample_params
+    redirect_to samples_path
   end
 
   def show
   end
 
-  def index
-    @samples = @current_user.samples
+  def edit
   end
 
-  def edit
+  def destroy
+
+  end
+
+  private
+  def sample_params
+    params.require(:sample).permit(:title, :audio_file, :image, :sample_type, :collection_id)
   end
 end
