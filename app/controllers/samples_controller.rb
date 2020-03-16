65628fbe6c080769470764e187f5b384ea0cb4ef
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-03-16 13:06:50 +1100

    Added new sample form layout, does not yet function

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -1,11 +1,19 @@
 class SamplesController < ApplicationController
+  before_action :check_for_login
+
   def new
+    @sample = Sample.new
+  end
+
+  def create
+    
   end
 
   def show
   end
 
   def index
+    @samples = @current_user.samples
   end
 
   def edit
