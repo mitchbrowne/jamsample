f32e0d40959ebc5ca1786b22ff92a4c1da75c31e
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-03-16 18:46:01 +1100

    Added link from welcome page to collections, added security to collection control bar

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -13,7 +13,7 @@ class CollectionsController < ApplicationController
 
   def show
     @collection = Collection.find params[:id]
-    check_for_collection
+    # check_for_collection
   end
 
   def index
