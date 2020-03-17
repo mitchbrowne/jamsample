0cb15444beaa1a21be153ff8974df8c1ae0d9e96
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-03-17 17:41:01 +1100

    Added cloudinary gem and api key security

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ gem 'jquery-rails'
 
 gem 'chosen-rails'
 
+gem 'cloudinary'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'pages#welcome'
-  resources :users, :only => [:new, :create, :index]
+  resources :users, :only => [:new, :create, :index, :show]
 
   get '/login' => 'session#new'
   post '/login' => 'session#create'
