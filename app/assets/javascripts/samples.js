8de0963d39a025f6e7e9927e97377e2c6f6163fe
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-03-18 10:26:53 +1100

    Added play button functionality and progressbar

--- app/assets/javascripts/samples.js
+++ app/assets/javascripts/samples.js
@@ -2,28 +2,26 @@ $(document).ready(function () {
   if ($('.samples.show').length === 0) return;
     console.log("Welcome to sample");
 
-let songs = ["https://ia800309.us.archive.org/15/items/Dje-AShortLoopWithTheRc-20xl/Dje.mp3"]
-let poster = ["https://i.pinimg.com/originals/5d/97/b8/5d97b8478ab7fefd6af1b85225640e6d.jpg"]
 
 let $songTitle = $("#songTitle");
 let $fillBar = $('#fill');
 
 let song = new Audio();
-let currentSong = 0; //points to current song
 
 window.onload = setSong; //calls function playSong when window loads
 
 function setSong() {
   song.src = $('.hidden_audio_file').text(); //set the source of 0th song
+  console.log(song.src)
 }
 
 function playOrPauseSong() {
   if(song.paused){
     song.play();
-    $("#play img").attr("src","https://raw.githubusercontent.com/DaftCreation/Custom-Audio-Player/master/Pause.png");
+    $('#play').removeClass('player_play').addClass('player_pause');
   } else{
     song.pause();
-    $("#play img").attr("src", "https://raw.githubusercontent.com/DaftCreation/Custom-Audio-Player/master/Play.png")
+    $('#play').removeClass('player_pause').addClass('player_play');
   };
 };
 
@@ -32,10 +30,13 @@ $('#play').click(function() {
   playOrPauseSong();
 });
 
+
+
 song.ontimeupdate = function() {
-  let position = song.currentTime / song.duration;
-  $fillBar.css('width',`${position * 100}%`);
-  // $fillBar.style.width = position * 100 +'%';
+  $('#seekbar').attr('value', song.currentTime / song.duration);
+  console.log(song.currentTime / song.duration)
+  // let position = song.currentTime / song.duration;
+  // $fillBar.css('width',`${position * 100}%`);
 };
 
 function replay() {
@@ -47,9 +48,5 @@ $('#pre').click(function() {
   console.log("YOU")
   replay();
 })
-
-
-
-
 
 });
