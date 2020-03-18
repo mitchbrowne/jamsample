cce8eaeb0692d64d04159583505b0969adc3e4de
Mitchell Browne <mitchellbrowne@Mitchells-MacBook-Air-2.local>
2020-03-18 20:38:43 +1100

    Added sample_id to show sample js page

--- app/assets/javascripts/samples.js
+++ app/assets/javascripts/samples.js
@@ -1,90 +1,100 @@
 $(document).ready(function () {
   if ($('.samples.show').length === 0) return;
 
-function calculateTotalValue(length) {
-  var minutes = Math.floor(length / 60),
-    seconds_int = length - minutes * 60,
-    seconds_str = seconds_int.toString(),
-    seconds = seconds_str.substr(0, 2),
-    time = minutes + ':' + seconds
+  function calculateTotalValue(length) {
+    var minutes = Math.floor(length / 60),
+      seconds_int = length - minutes * 60,
+      seconds_str = seconds_int.toString(),
+      seconds = seconds_str.substr(0, 2),
+      time = minutes + ':' + seconds
 
-  return time;
-}
+    return time;
+  }
 
-function calculateCurrentValue(currentTime) {
-  var current_hour = parseInt(currentTime / 3600) % 24,
-    current_minute = parseInt(currentTime / 60) % 60,
-    current_seconds_long = currentTime % 60,
-    current_seconds = current_seconds_long.toFixed(),
-    current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
+  function calculateCurrentValue(currentTime) {
+    var current_hour = parseInt(currentTime / 3600) % 24,
+      current_minute = parseInt(currentTime / 60) % 60,
+      current_seconds_long = currentTime % 60,
+      current_seconds = current_seconds_long.toFixed(),
+      current_time = (current_minute < 10 ? "0" + current_minute : current_minute) + ":" + (current_seconds < 10 ? "0" + current_seconds : current_seconds);
 
-  return current_time;
-}
+    return current_time;
+  }
 
-function initPlayer() {
-
-  //VARIABLES
-  let playerContainer = $('#audio_wrapper');
-  let player = $('#player');
-  let isPlaying = false;
-  let playButton = $('#play');
-
-  //CONTROL LISTENERS
-  if (playButton != null) {
-    playButton.click(function() {
-      togglePlay()
-    });
-  };
-
-  //CONTROLS & SOUNDS METHODS
-  function togglePlay() {
-    if (player.get(0).paused === false) {
-      player.get(0).pause();
-      isPlaying = false;
-      $('#play').removeClass('player_pause').addClass('player_play');
-    } else {
-      player.get(0).play();
-      $('#play').removeClass('player_play').addClass('player_pause');
-      isPlaying = true;
+  function initPlayers() {
+    for (let i = 0; i < $sample_ids.length; i++) {
+      initPlayer($sample_ids[i].innerHTML);
     }
   }
 
-  //PROGRESS BAR ONTIMEUPDATE
-  function initProgressBar() {
-    let length = player.get(0).duration;
-    let current_time = player.get(0).currentTime;
+  function initPlayer(sample_id) {
 
-    //calculate total length of value
-    let totalLength = calculateTotalValue(length);
-    $('.end-time').html(totalLength);
+    console.log(sample_id);
 
-    //calculate current value time
-    currentTime = calculateCurrentValue(current_time);
-    $('.start-time').html(currentTime);
+    //VARIABLES
+    let playerContainer = $(`#audio_wrapper${sample_id}`);
+    let player = $(`#player${sample_id}`);
+    let isPlaying = false;
+    let playButton = $(`#play${sample_id}`);
 
-    let progressBar = $('#seekbar');
-    progressBar.attr('value', player.get(0).currentTime / player.get(0).duration);
-    console.log(progressBar.value)
-    progressBar.click(seek);
-
-    if (player.get(0).currentTime == player.get(0).duration) {
-      $('#play').removeClass('player_pause').addClass('player_play');
-    }
-
-    function seek(evt) {
-      let percent = evt.offsetX / this.offsetWidth;
-      player.get(0).currentTime = percent * player.get(0).duration;
-      progressBar.value = percent / 100;
+    //CONTROL LISTENERS
+    if (playButton != null) {
+      playButton.click(function() {
+        togglePlay()
+      });
     };
 
+    //CONTROLS & SOUNDS METHODS
+    function togglePlay() {
+      if (player.get(0).paused === false) {
+        player.get(0).pause();
+        isPlaying = false;
+        $(`#play${sample_id}`).removeClass('player_pause').addClass('player_play');
+      } else {
+        player.get(0).play();
+        $(`#play${sample_id}`).removeClass('player_play').addClass('player_pause');
+        isPlaying = true;
+      }
+    }
+
+    //PROGRESS BAR ONTIMEUPDATE
+    function initProgressBar() {
+      let length = player.get(0).duration;
+      let current_time = player.get(0).currentTime;
+
+      //calculate total length of value
+      let totalLength = calculateTotalValue(length);
+      $(`.end-time`).html(totalLength);
+
+      //calculate current value time
+      currentTime = calculateCurrentValue(current_time);
+      $(`.start-time`).html(currentTime);
+
+      let progressBar = $(`#seekbar${sample_id}`);
+      progressBar.attr('value', player.get(0).currentTime / player.get(0).duration);
+      progressBar.click(seek);
+
+      if (player.get(0).currentTime == player.get(0).duration) {
+        $(`#play${sample_id}`).removeClass('player_pause').addClass('player_play');
+      }
+
+      function seek(evt) {
+        let percent = evt.offsetX / this.offsetWidth;
+        player.get(0).currentTime = percent * player.get(0).duration;
+        progressBar.value = percent / 100;
+      };
+
+    };
+
+    player.on('timeupdate', function() {
+      initProgressBar()
+    });
   };
 
-  player.on('timeupdate', function() {
-    initProgressBar()
-  });
-};
+  // extract all sample_ids from html marketplace page
+  let $sample_ids = $('.hidden_id').toArray();
 
-// initialise player when page has loaded
-initPlayer();
+// initialise players when page has loaded
+  initPlayers();
 
 });
